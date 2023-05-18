6eddec64b780700e03c6a199a90dc29b4a120872
Hammes Allan <hammes.alln@gmail.com>
2023-05-18 21:42:05 +0200

    implement begin of promotion system
    	renommé :         src/mercadona/templates/mercadona/promotion.html -> src/mercadona/templates/mercadona/promo_new.html
    	modifié :         src/mercadona/urls.py
    	modifié :         .idea/vcs.xml
    	modifié :         src/mercadona/__pycache__/admin.cpython-311.pyc
    	modifié :         src/mercadona/__pycache__/forms.cpython-311.pyc
    	modifié :         src/mercadona/__pycache__/models.cpython-311.pyc
    	modifié :         src/mercadona/__pycache__/urls.cpython-311.pyc
    	modifié :         src/mercadona/__pycache__/views.cpython-311.pyc
    	modifié :         src/mercadona/admin.py
    	modifié :         src/mercadona/forms.py
    	modifié :         src/mercadona/migrations/__pycache__/0017_produit_promotion.cpython-311.pyc
    	modifié :         src/mercadona/migrations/__pycache__/0018_promotion.cpython-311.pyc
    	modifié :         src/mercadona/migrations/__pycache__/0019_remove_produit_durer_remove_produit_promotion.cpython-311.pyc
    	modifié :         src/mercadona/migrations/__pycache__/0021_promotion_promo_id.cpython-311.pyc
    	modifié :         src/mercadona/models.py
    	modifié :         src/mercadona/templates/mercadona/base.html
    	modifié :         src/mercadona/templates/mercadona/list.html
    	modifié :         src/mercadona/templates/mercadona/produit_new.html
    	modifié :         src/mercadona/urls.py
    	modifié :         src/mercadona/views.py
    	src/mercadona/migrations/0024_delete_promotion.py
    	src/mercadona/migrations/0025_promotion.py
    	src/mercadona/migrations/0026_alter_promotion_id_product.py
    	src/mercadona/migrations/0027_alter_promotion_id_product.py
    	src/mercadona/migrations/0028_remove_promotion_id_product.py
    	src/mercadona/migrations/__pycache__/0022_remove_promotion_promo_id.cpython-311.pyc
    	src/mercadona/migrations/__pycache__/0023_promotion_promo_id.cpython-311.pyc
    	src/mercadona/migrations/__pycache__/0024_delete_promotion.cpython-311.pyc
    	src/mercadona/migrations/__pycache__/0025_promotion.cpython-311.pyc
    	src/mercadona/migrations/__pycache__/0026_alter_promotion_id_product.cpython-311.pyc
    	src/mercadona/migrations/__pycache__/0027_alter_promotion_id_product.cpython-311.pyc
    	src/mercadona/migrations/__pycache__/0028_remove_promotion_id_product.cpython-311.pyc
    	"src/mercadona/static/mercadona/images/Capture_d\303\251cran_du_2023-05-08_16-54-12_R6pujg9.png"
    	"src/mercadona/static/mercadona/images/Capture_d\303\251cran_du_2023-05-10_23-59-05.png"

--- src/mercadona/urls.py
+++ src/mercadona/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('logout', views.logout_view, name="logout"),
     path('delete/<product_id>', views.delete_product, name="delete"),
     path('produit/new/', views.produit_new, name='produit_new'),
-    path('promotion/<promo_ID>', views.promotions, name='promotion'),
+    path('promotion/<product_id>', views.promotions, name='promotion'),
 
 
 ]
